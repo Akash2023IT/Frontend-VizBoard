6251a9b0b60051adca4c393c612247d2622fbf05
Akash Dhankar <akashdhankar.2023it@sece.ac.in>
2025-01-30 21:49:27 +0530

    save

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://backend-vizboard-1.onrender.com/api';
+export const API_BASE_URL = 'https://backend-vizboard-1.onrender.com';
 
 export const API_ENDPOINTS = {
     SIGNUP: '/api/auth/signup',
