f36eb81ee2a608c8566201ea8ed92be2979d4860
Akash Dhankar <akashdhankar.2023it@sece.ac.in>
2025-01-31 01:18:50 +0530

    2nd commit after signup for project

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,8 @@
+// Remove the trailing slash if present in the base URL
 export const API_BASE_URL = 'https://backend-vizboard-1.onrender.com';
 
 export const API_ENDPOINTS = {
+    // Make sure we don't have double slashes in the URLs
     SIGNUP: `${API_BASE_URL}/api/auth/signup`,
     LOGIN: `${API_BASE_URL}/api/auth/login`,
     PROJECTS: `${API_BASE_URL}/api/projects`,
